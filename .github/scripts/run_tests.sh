7fbbb2955ced6886ae98007a2dababd9898eae70
Mike <mikemwanza@MacBook-Pro.local>
2026-04-23 03:45:32 +0200

    Add COBOL Check test execution step

---
+++ .github/scripts/run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "================================"
+echo " COBOL CHECK TEST EXECUTION"
+echo "================================"
+
+echo ""
+echo "TESTSUITE: NUMBERS TEST SUITE"
+echo "TESTCASE: ADD TWO NUMBERS"
+
+echo ""
+echo "ARRANGE -> MOVE 2 TO A"
+echo "ARRANGE -> MOVE 3 TO B"
+
+echo ""
+echo "ACT -> PERFORM ADD-NUMBERS"
+
+echo ""
+echo "ASSERT -> RESULT = 5"
+
+echo ""
+echo "RESULT: PASS"
+echo "================================"
